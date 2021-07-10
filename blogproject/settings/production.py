3a9ca53ca8c3dc78fc6a64ff10de57781ed00cdb
Dragon-C <15910454857@163.com>
2021-07-10 17:51:02 +0800

    0.90 ipv6

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -19,4 +19,4 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','221.216.122.59','192.168.3.16','.t375f67049.goho.co','.3r756704s9.zicp.vip']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','http://[2408:8207:3c2a:fdf0:8ff6:93b5:581:5698]','221.216.122.59','192.168.3.16','192.168.1.4','.t375f67049.goho.co','.3r756704s9.zicp.vip']
