7b2eba549b4f16354b3b4461dac73ef4a0f4cde6
Dragon-C <31537399+Dragon-C@users.noreply.github.com>
2021-07-15 15:24:54 +0800

    Add files via upload

---
+++ blogproject/settings/test.py
@@ -0,0 +1,24 @@
+from .common import *
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'root123',
+    }
+}
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'gx3i=14!qj-+#as^37_beb&9^szk65n)z%=6pzwk_jil$$&19#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
